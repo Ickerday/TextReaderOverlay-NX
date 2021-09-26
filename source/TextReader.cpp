0248fbe0f571f2ba0c1191bb8e8e931ec7762b1e
tungns <tungns0695@gmail.com>
2021-09-26 16:27:50 +0700

    change font path

--- source/TextReader.cpp
+++ source/TextReader.cpp
@@ -53,8 +53,7 @@ TextReader::TextReader(std::string const &path)
       m_chunkMid(0),
       m_loading(false),
       m_loaded(false),
-      m_font("sdmc:/switch/.overlays/TextReaderOverlay/fonts/UbuntuMono/UbuntuMono-Regular.ttf"),
-      m_size(10),
+      m_font("sdmc:/switch/.overlays/TextReaderOverlay/fonts/font.ttf"),
       m_panx(0),
       m_debug(true)
 {
